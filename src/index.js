6059868c124aab784f04c8218f97b01d9c9f2bb1
Sohaib gour <76823175+goriens@users.noreply.github.com>
2022-03-27 21:42:32 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const connect = require("./configs/db");
+const { register, login } = require("./controllers/auth.controllers");
+const userController = require("./controllers/users.controllers");
+const postController = require("./controllers/post.controllers");
+const { body, validationResults } = require("express-validator");
+
+
+app.post("/register",
+    body("name")
+        .trim()
+        .not()
+        .isEmpty()
+        .withMessage("Please enter your name"),
+    body("email")
+        .trim()
+        .not()
+        .isEmpty()
+        .withMessage("Please enter a valid email")
+        .isEmail(),
+    body("password")
+        .trim()
+        .not()
+        .isEmpty(),
+    register);
+app.post("/login",
+    body("email")
+        .trim()
+        .not()
+        .isEmpty()
+        .withMessage("Please enter a valid email")
+        .isEmail(),
+    body("password")
+        .trim()
+        .not()
+        .isEmpty(),
+    login);
+
+app.use("/users", userController);
+app.use("/post", postController);
+
+app.listen(5000, async () => {
+    try {
+        await connect();
+        console.log("Listening on port 5000");
+    } catch (error) {
+        console.log(error);
+    }
+});
